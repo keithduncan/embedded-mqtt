9df6452f05ec39a5102a90386a7d2acc8f1e710a
Keith Duncan <keith_duncan@me.com>
2019-05-30 12:19:09 +1000

    Convert qos to spaces

--- src/qos.rs
+++ src/qos.rs
@@ -1,6 +1,6 @@
 use core::{
-	convert::{TryFrom, From},
-	result::Result,
+    convert::{TryFrom, From},
+    result::Result,
 };
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
@@ -12,30 +12,30 @@ pub enum QoS {
 
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
 pub enum Error {
-	BadPattern,
+    BadPattern,
 }
 
 impl TryFrom<u8> for QoS {
-	type Error = Error;
-	
-	fn try_from(byte: u8) -> Result<QoS, Error> {
-		let qos = match byte & 0b11 {
-			0b00 => QoS::AtMostOnce,
-			0b01 => QoS::AtLeastOnce,
-			0b10 => QoS::ExactlyOnce,
-			_ => return Err(Error::BadPattern),
-		};
+    type Error = Error;
+    
+    fn try_from(byte: u8) -> Result<QoS, Error> {
+        let qos = match byte & 0b11 {
+            0b00 => QoS::AtMostOnce,
+            0b01 => QoS::AtLeastOnce,
+            0b10 => QoS::ExactlyOnce,
+            _ => return Err(Error::BadPattern),
+        };
 
-		Ok(qos)
-	}
+        Ok(qos)
+    }
 }
 
 impl From<QoS> for u8 {
-	fn from(qos: QoS) -> u8 {
-		match qos {
-			QoS::AtMostOnce  => 0b00,
-			QoS::AtLeastOnce => 0b01,
-			QoS::ExactlyOnce => 0b10,
-		}
-	}
+    fn from(qos: QoS) -> u8 {
+        match qos {
+            QoS::AtMostOnce  => 0b00,
+            QoS::AtLeastOnce => 0b01,
+            QoS::ExactlyOnce => 0b10,
+        }
+    }
 }
